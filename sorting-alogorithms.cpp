b2cb8e5dcd3083d3fe269909cd5e018ea75ee062
Akhil Chandail <akhilchandail9@gmail.com>
2024-03-23 01:41:46 +0530

    Update sorting-alogorithms.cpp

--- sorting-alogorithms.cpp
+++ sorting-alogorithms.cpp
@@ -70,3 +70,59 @@ void insertionSort(int arr[], int n)
 
     // Insert last at its correct position
    
+//Merge Sort:
+#include<bits/stdc++.h>
+#include<vector>
+using namespace std;
+
+void merge(int arr[], int l, int mid, int r){
+    vector<int>temp;
+    int left=l;
+    int right=mid+1;
+
+    while(left<=mid && right <=r){
+        if(arr[left]<=arr[right]){
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else{
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+
+    while(left<=mid){
+            temp.push_back(arr[left]);
+            left++;
+    }
+
+    while(right<=r){
+            temp.push_back(arr[right]);
+            right++;
+    }
+
+    for(int i =l;i<=r;i++){
+        arr[i]=temp[i-l];
+    }
+}
+
+void mergeSort(int arr[], int l, int r) {
+
+    if(l>=r){
+        return;
+    }
+    int mid = (l+r)/2;
+    mergeSort(arr,l,mid);
+    mergeSort(arr,mid+1,r);
+    merge(arr,l,mid,r);
+}
+
+void mergeSort(vector<int>& arr, int l, int r) {
+    int n = arr.size();
+    int* arrPtr = &arr[0];
+    mergeSort(arrPtr, l, r);
+}
+//Space complexity: O(n)  
+//Time complexity: O(nlogn) 
+
+
